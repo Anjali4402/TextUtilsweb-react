fed168c2660aee55e607c6e98f2e15ebb113f523
Anjali <anjalinegian2002@gmail.com>
2022-09-10 20:52:45 +0530

    first- add name in description and change name of title of file... second -- install react router dom and make our file better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import React from 'react';
 // import logo from './logo.svg';
 
-// import About from "./components/About";
+import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 function App() {
   const [mode , setMode] = useState("light");
@@ -18,7 +24,7 @@ function App() {
       })
       setTimeout(() => {
         setAlert(null);
-      }, 5000);
+      }, 2000);
   }
 
   const toggleMode = ()=>{
@@ -26,21 +32,27 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor="#042743";
       showAlert('Dark mode has been enable','success');
+      document.title = 'TextUtils - Dark Mode'
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert('light mode has been enable','success');
+      document.title = 'TextUtils - Light Mode'
     }
   }
   return (
     <>
+    <Router>
       <Navbar title="TextUtils"  mode={mode} toggleMode = {toggleMode}/>
       <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading='Enter a text to analyze below' mode={mode} upperAlert={showAlert}/>
-        {/* <About/> */}
+        <Routes>
+            <Route exact path="/about" element={<About/>}></Route>
+            <Route exact path="/" element={<TextForm heading='Enter a text to analyze below' mode={mode} upperAlert={showAlert}/>}></Route>
+        </Routes>
         </div>
+    </Router>
     </>
   )
 }
